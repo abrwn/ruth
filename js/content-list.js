287be50a3228b0668aae6405b79a3ae76ed99493
abrwn <abr@live.co.uk>
2018-12-21 10:00:41 +0000

    Update content list and new profile

--- js/content-list.js
+++ js/content-list.js
@@ -5,12 +5,13 @@ var paintDetail = [
 { title: "Clay Heads", desc: "140 x 170cm, oil on canvas", href: "clay-heads" },
 { title: "Katamari", desc: "120 x 180cm, oil on canvas", href: "katamari" },
 { title: "The Croft", desc: "140 x 170cm, oil on canvas", href: "the-croft" },
+{ title: "Marine Fauna", desc: "Oil on canvas, 50cm x 80cm", href: "fish" },
 { title: "Bumhead", desc: "140 x 170cm, oil on canvas", href: "bumhead" },
-{ title: "Fluffy Bunnies", desc: "80 x 100cm (x2), oil on canvas", href: "fluffy-bunnies" },
 { title: "White China Cats", desc: "150 x 150cm, oil on canvas", href: "white-china-cats" },
 { title: "Atom Smashing", desc: "150 x 210cm, oil on canvas", href: "atom-smashing" },
 { title: "The Accordionist", desc: "80 x 100cm, oil on canvas", href: "the-accordionist" },
 { title: "Sur Le Fil", desc: "150 x 210cm, oil on canvas", href: "sur-le-fil" },
+{ title: "Rope", desc: "oil on canvas, 190 x 210cm", href: "Rope" },
 { title: "The Debutantes", desc: "150 x 150cm, oil on canvas", href: "the-debutantes" },
 { title: "Tessa with Watermelon", desc: "100 x 120cm, oil on canvas", href: "tessa-with-watermelon" },
 { title: "Obby Oss", desc: "150 x 210cm, oil on canvas", href: "obby-oss" },
@@ -23,11 +24,9 @@ var paintDetail = [
 { title: "Untitled (Hammock)", desc: "150 x 210cm, oil on canvas", href: "untitled-hammock" },
 { title: "Spam Man", desc: "140 x 170cm, oil on canvas", href: "spam-man" },
 { title: "Hands", desc: "120 x 140cm, oil on canvas", href: "hands" },
-//{ title: "", desc: "cm, oil on canvas", href: "" },
-//Insert new painting rows on a new line below here
-
-
-//Insert new rows above here
+{ title: "Canopy", desc: "oil on canvas, 160 x 190cm", href: "Canopy" },
+{ title: "Duff", desc: "Oil on canvas, 150 x 210cm", href: "duff" },
+//Insert new rows on a new line above here ^
 ];
 
 
@@ -37,18 +36,30 @@ var paintDetail = [
 var smallPaintDetail = [
 { title: "Tessa", desc: "30 x 40cm, oil on canvas", href: "tessa" },
 { title: "But-Tub", desc: "30 x 40cm, oil on linen", href: "but-tub" },
+{ title: "Erica", desc: "oil on linen, 30 x 40cm", href: "Erica" },
+{ title: "Anna", desc: "oil on linen, 30 x 40cm", href: "Anna-head" },
 { title: "Ascension", desc: "40 x 50cm, oil on canvas", href: "ascension" },
+{ title: "Sister Sister", desc: "oil on board, 30 x 20cm", href: "sister-sister" },
 { title: "Bananas", desc: "30 x 40cm (x2), oil on canvas", href: "bananas" },
 { title: "Meat Heads", desc: "25 x 35cm, oil on canvas", href: "meat-heads" },
 { title: "Sausages", desc: "50 x 50cm, oil on canvas", href: "sausages" },
+{ title: "Skomakerdiket", desc: "oil on board, 30 x 40cm", href: "Skomakerdiket" },
 { title: "Hairy Shapes", desc: "30 x 40cm, 50 x 70cm, oil on canvas", href: "hairy-shapes" },
 { title: "Watermelon", desc: "50 x 38cm, oil on canvas", href: "watermelon" },
+{ title: "Grip Grass", desc: "oil on wood panel, 30 x 40cm", href: "Grippgrass" },
+{ title: "Duff", desc: "oil on linen, 30 x 40cm", href: "Duff-small" },
 { title: "Mirror", desc: "30 x 40cm, oil on canvas", href: "mirror" },
 { title: "Shrub", desc: "30 x 40cm, oil on canvas", href: "shrub" },
+{ title: "Georgina", desc: "oil on linen, 30 x 50cm", href: "Georgina" },
 { title: "Balloon Animal", desc: "30 x 40cm, oil on canvas", href: "balloon-animal" },
 //{ title: "", desc: "cm, oil on canvas", href: "" },
 //Insert new small painting rows on a new line below here
 { title: "Clap Clap Flap", desc: "Oil on Canvas, 50 x 80cm", href: "clap-clap-flap" },
+{ title: "The Offering no.1", desc: "oil on linen", href: "mud-series-1" },
+{ title: "The Offering no.4", desc: "oil on linen, 30 x 40cm", href: "mud-series-4" },
+{ title: "The Offering", desc: "oil on linen and clay", href: "mud-series" },
+{ title: "Ailah Stretching (installation view)", desc: "oil on linen, wall paint and ceramic", href: "Ailah-Stretching" },
+{ title: "Untitled", desc: "oil on panel, ceramic", href: "Hanky-eggs" },
 
 //Insert new rows above here
 ];
@@ -66,12 +77,24 @@ var paperDetail = [
 { title: "Untitled", desc: "Coloured pencils on card", href: "card-3" },
 { title: "Untitled", desc: "Coloured pencils on card", href: "card-4" },
 { title: "Untitled", desc: "Collage", href: "collage-1" },
+{ title: "Charlotte", desc: "collaged paper, coloured crayons pens, cork and glazed ceramic, 46 x 70cm", href: "Charlotte" },
 { title: "Untitled", desc: "Collage", href: "collage-2" },
 { title: "Untitled", desc: "Collage", href: "collage-3" },
 { title: "Untitled", desc: "Collage", href: "collage-4" },
 { title: "Untitled", desc: "Collage", href: "collage-5" },
 { title: "Untitled", desc: "Oil on card, postcard", href: "oil-card-1" },
 { title: "Untitled", desc: "Oil on card", href: "oil-card-2" },
+{ title: "Untitled", desc: "oil on card", href: "Oil-Bennet-Postcard" },
+{ title: "Untitled", desc: "Collage", href: "collage-shadow" },
+{ title: "Untitled", desc: "Collage", href: "Scan" },
+{ title: "Untitled", desc: "Collage", href: "Scan-copy-9" },
+{ title: "Untitled", desc: "Collage", href: "Scan-1" },
+{ title: "Untitled", desc: "Collage", href: "Scan-copy-2" },
+{ title: "Untitled", desc: "Collage", href: "collage-table" },
+{ title: "Untitled", desc: "Paper collage on cork", href: "Helena-greenhouse-collage" },
+{ title: "Helena's Hat", desc: "cork and paper collage", href: "Helenas-hat-collages" },
+{ title: "Ailah-Collage", desc: "coloured paper, crayons, pens, shells and glazed ceramic", href: "Palms" },
+
 //Insert new paper rows on a new line below here
 
 
